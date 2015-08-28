97bb47e633958ab1cf2fc8ea6078c64cbe5cb504
Benedikt Ritter <beneritter@gmail.com>
2015-08-28 10:31:27 +0200

    Don't remove site dir

--- cleanup.sh
+++ cleanup.sh
@@ -3,4 +3,4 @@
 docker rm $(docker ps -aq)
 rm -rf mounted_directories/jenkins_home/*
 rm -rf mounted_directories/jenkins_home/.jenkins
-rm -rf mounted_directories/site_deploy/*
+rm -rf mounted_directories/site_deploy/site/*
