0322ea9a956e5bd5739bcfd784f2719c165bcd38
Benedikt Ritter <beneritter@gmail.com>
2015-08-28 08:37:41 +0200

    Write web context to worblehat configuration on application startup

--- docker/worblehat-host/init_env.sh
+++ docker/worblehat-host/init_env.sh
@@ -2,6 +2,7 @@
 
 # the sshd does not see environment variables set by docker
 # we write the SPRING_PROFILES_ACTIVE env var to the profile after start up
-echo "export SPRING_PROFILES_ACTIVE=$SPRING_PROFILES_ACTIVE" >> /etc/profile
+echo "# Spring WebApp Config" >> /opt/worblehat/application.properties
+echo "server.context-path=/worblehat-$SPRING_PROFILES_ACTIVE" >> /opt/worblehat/application.properties
 
 /usr/sbin/sshd -D
