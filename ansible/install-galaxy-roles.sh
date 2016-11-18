9074952db46c9755573f13f820d746bba4f103a4
Andreas Ebbert-Karroum <andreas@karroum.de>
2016-11-18 14:33:57 +0100

    raise jenkins version

--- ansible/install-galaxy-roles.sh
+++ ansible/install-galaxy-roles.sh
@@ -4,5 +4,5 @@ ansible-galaxy install -p galaxy_roles \
   angstwad.docker_ubuntu,v1.1.1 \
   jdauphant.nginx,v1.11.2 \
   geerlingguy.java,1.3.0 \
-  geerlingguy.jenkins,1.3.0 \
+  geerlingguy.jenkins,2.6.0 \
   geerlingguy.mysql,1.9.1
