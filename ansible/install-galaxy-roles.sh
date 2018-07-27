01f7b30bdc919e20349dd59e0777612832cb3fdb
Benedikt Ritter <benedikt.ritter@codecentric.de>
2018-07-27 13:13:12 +0200

    Update Ansible roles

--- ansible/install-galaxy-roles.sh
+++ ansible/install-galaxy-roles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ansible-galaxy install -p galaxy_roles \
-  jdauphant.nginx,v2.5 \
-  geerlingguy.java,1.3.0 \
-  geerlingguy.jenkins,2.6.0 \
-  geerlingguy.mysql,2.4.2
+  jdauphant.nginx,v2.18.1 \
+  geerlingguy.java,1.8.1 \
+  geerlingguy.jenkins,3.5.0 \
+  geerlingguy.mysql,2.9.0
