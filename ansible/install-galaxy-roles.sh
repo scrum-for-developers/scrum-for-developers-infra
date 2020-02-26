83185dfee9924c1e222738956f491294dcb423a0
Andreas Ebbert-Karroum <andreas@karroum.de>
2020-02-26 18:06:15 +0100

    change ports for test and prod, add docker

--- ansible/install-galaxy-roles.sh
+++ ansible/install-galaxy-roles.sh
@@ -4,4 +4,5 @@ ansible-galaxy install -p galaxy_roles \
   jdauphant.nginx,v2.21.2 \
   geerlingguy.jenkins,3.8.0 \
   geerlingguy.mysql,2.9.5 \
-  geerlingguy.postgresql,2.0.1
+  geerlingguy.postgresql,2.0.1 \
+  geerlingguy.docker,2.6.1
