25ffd6fb454920cf02433c1eda3a9cca7f528839
Andreas Ebbert-Karroum <andreas@karroum.de>
2020-03-04 18:25:23 +0100

    Fix create playbook

--- ansible/create-ec2-instances.sh
+++ ansible/create-ec2-instances.sh
@@ -9,7 +9,17 @@ else
   INVENTORY=inventory-localhost
 fi
 
+if [ "$#" -ne 1 ]
+then
+  echo "Usage $0 <private-key>"
+  exit 1
+fi
+
+PRIVATE_KEY=$1
+echo "Using private key $PRIVATE_KEY"
 ansible-playbook \
   --connection=local \
   --inventory-file=$INVENTORY \
+  -u ubuntu \
+  --private-key ${PRIVATE_KEY} \
   create-ec2-playbook.yml
